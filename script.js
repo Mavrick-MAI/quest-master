c95e6f20d2533032739e223acad23e5713a7c1b3
Mavrick-MAI <112839299+Mavrick-MAI@users.noreply.github.com>
2024-12-30 12:02:25 +1100

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+    // Create a Vue app
+    const app = Vue.createApp({
+        data() {
+          return {
+            message: 'Hello, World!'
+          };
+        }
+      });
+  
+      // Mount the app
+      app.mount('#app');
