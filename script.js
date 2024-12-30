e4b4f4b978dc3d0a36a1b24d2a088012047b5555
Mavrick-MAI <mavrickmaivany@outlook.fr>
2024-12-30 12:40:18 +1100

    Test grid

--- script.js
+++ script.js
@@ -3,8 +3,16 @@
     const app = Vue.createApp({
         data() {
           return {
-            message: 'Hello, World!'
+            gridSize: 10,
+            playerPosition: { row: 0, col: 0 }, // Player starts at top-left corner
           };
+        },
+        computed: {
+          grid() {
+            return Array.from({ length: this.gridSize }, (_, row) => 
+              Array.from({ length: this.gridSize }, (_, col) => `${row},${col}`)
+            );
+          }
         }
       });
   
