9464718db74b8d753a3e66243d6ae1e51c2f5ecf
Mavrick-MAI <mavrickmaivany@outlook.fr>
2025-01-27 19:29:27 +1100

    player setup in the grid

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 import Player from './model/Player.js';
 
+// Variables
+let grid;
+let player;
+
+let newGameButton = document.getElementById("newGameButton");
+newGameButton.addEventListener("click", () => createGrid(10));
+
 function createGrid(gridSize) {
     
-    let grid = document.getElementById("grid");
+    grid = document.getElementById("grid");
     while (grid.lastElementChild) {
         grid.removeChild(grid.lastElementChild);
     }
 
-    let player = new Player(Math.random() * 9, Math.random() * 9, 20, 5);
+    player = new Player(Math.floor(Math.random() * 10), Math.floor(Math.random() * 10), 20, 5);
 
     console.log(player)
     let newCell;
-    for (i = 0; i < gridSize; i++) {
-        for (j = 0; j < gridSize; j++) {
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
 
             newCell = document.createElement("div");
             newCell.dataset.x = i;
             newCell.dataset.y = j
             newCell.className = "cell";
-            grid.appendChild(newCell);
+            if (i === player.x && j === player.y) {
+                newCell.innerHTML = "<i id='playerIcon' class='fa-solid fa-person-walking fa-2xl'></i>";
+            }
+            grid.append(newCell);
         }
     }
 }
